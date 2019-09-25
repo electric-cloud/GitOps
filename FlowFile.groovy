f19329da1d7ec727178ff5c0f6d25fcdfa04ffdf
Greg Maxey <gregorymaxey@yahoo.com>
2019-09-25 12:25:56 -0400

    Update FlowFile.groovy

--- FlowFile.groovy
+++ FlowFile.groovy
@@ -1,3 +1,8 @@
 project "Test GitOps",{
   description = "Example generated project from FlowFile"
+  pipeline "Test Pipeline",{
+    stage "QA"
+    stage "UAT"
+    stage "PROD"
+  } // afterStage:
 }
